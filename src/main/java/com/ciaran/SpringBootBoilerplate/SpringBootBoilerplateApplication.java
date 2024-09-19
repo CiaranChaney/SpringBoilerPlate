65d39273608dda0f4615ab034fd2be1ddcd03acc
Ciaran Chaney <ciaranchaney@Ciarans-MacBook-Air.local>
2024-09-19 20:47:06 +0100

    Initial commit for boiler plate

---
+++ src/main/java/com/ciaran/SpringBootBoilerplate/SpringBootBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.ciaran.SpringBootBoilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciaran/SpringBootBoilerplate/SpringBootBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciaran.SpringBootBoilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
